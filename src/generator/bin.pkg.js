016832cca24a74d4c49d985b4a2edf2a59c584de
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-26 22:01:17 +0700

    Update bin.pkg.js

--- src/generator/bin.pkg.js
+++ src/generator/bin.pkg.js
@@ -1,4 +1,13 @@
 const { existsSync, mkdirSync, writeFileSync } = require("fs");
 const path = require("path")
 
-if (existSync())
+const assets = path.join(__dirname, "../../assets");
+const packages = path.join(__dirname, "../../packages");
+
+if (existSync(assets)) return false;
+if (existSync(packages)) return false;
+
+mkdirSync(assets);
+mkdirSync(packages)
+
+// Write Downloader for github repository: Animation-Creator
