1d141fd5b66b9d3f89071a955e20ec929498196f
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-26 21:34:11 +0700

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,40 +1,26 @@
 /** CENTRALIZED PARSER */
 module.exports = async function centralParse(lines) {
     return lines
-        .filter(line => line.trim() && !line.startsWith("#") && !line.startsWith(undefined)) // Ignore empty lines and comments
+        .filter(line => line.trim() && !line.startsWith("#")) // Ignore empty lines and comments
         .map(line => {
             
             if (line.startsWith("@style")) {
-                return { type: "Style", style: line.match };
-            } else
-            
-
-            /** END STATEMENTS */
-            if (line.startsWith("@end")) {
-                return { type: "EndStatement" };
+                return { type: "Style", style: line.match(/@style (.+)/)?.[1] };
             } else
 
-            /** LOOP STATEMENTS */
-            if (line.startsWith("@while")) {
-                return { type: "WhileLoop", condition: line.match(/@while \[(.+?)\]:/)?.[1], body: [], index: line.match(/@while \[(.+?)\]:/)?.index };
+            if (line.startsWith("@title")) {
+                return { type: "Title", title: line.match(/@title (.+)/)?.[1] }
             } else
 
-            if (line.startsWith("@for")) {
-                return { type: "ForLoop", variable: line.match(/@for \[(.+?)\]/)?.[1], arguments: line.match(/@for \[(.+?)\] (.+)/)?.[2], range: line.match(/@for \[(.+?)\] (.+) \[(.+?)\]:/)?.[3], body: [], index: line.match(/@for \[(.+?)\] (.+) \[(.+?)\]:/)?.index };
+            if (line.startsWith("@lyrics")) {} else
+
+            if (line.atartsWith("@thumbnail")) {
+                return { type: "Thumbnail", prompt: line.match(/@thumbnail (.+)/)?.[1] }
             } else
 
-            /** OUTPUT STATEMENTS */
-            if (line.startsWith("@output")) {
-                return { type: "OutputStatement", value: line.match(/@output (.+)/)?.[1], line: line.match(/@output (.+)/)?.[0] };
-            }
-            
-            /** VARIABLE STATEMENTS */
-            if (line.startsWith("@var")) {
-                return { type: "Variable", name: line.match(/@var \[(.+?)\]:/)?.[1], param: line.match(/@var \[(.+?)\]: \((.+?)\)/)?.[2], value: line.match(/@var \[(.+?)\]: \((.+?)\)(.+)/)?.[3] };
-            } else
-
-            if (line.startsWith("@input")) {
-                return { type: "InputStatement", prompt: line.match(/@input \[(.+)\]:/)?.[1], variable: line.match(/@input \[(.+)\]: (.+)/)?.[2] };
+            if (line.startsWith("@desc")) {
+                // Also use as an style prompt.
+                return { type: "Description", message: line.match(/@desc (.+)/)?.[1] }
             }
 
             /** GENERIC STATEMENTS */
