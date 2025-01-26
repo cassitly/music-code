c8cf8e163abb7df082e786a7c039df3713e94ab7
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-26 22:06:00 +0700

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -12,7 +12,10 @@ module.exports = async function centralParse(lines) {
                 return { type: "Title", title: line.match(/@title (.+)/)?.[1] }
             } else
 
-            if (line.startsWith("@lyrics")) {} else
+            if (line.startsWith("@lyrics")) {
+                // Add Logic for lyrics
+                return { type: "Lyrics", parameters: line.match(/@lyrics (.+)/)?.[1] }
+            } else
 
             if (line.atartsWith("@thumbnail")) {
                 return { type: "Thumbnail", prompt: line.match(/@thumbnail (.+)/)?.[1] }
