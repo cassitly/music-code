6ea000516ac3e302e2897d297d32b7a632409b54
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-26 21:39:49 +0700

    Update runner.js

--- src/runner.js
+++ src/runner.js
@@ -1,1 +1,17 @@
-const centralParse = require()
+const centralParse = require("./parser.js")
+
+// Main Compiler Function
+async function action(lines) {
+    const main = centralParse(lines);
+    // await parseScript(main);
+
+    // Output for Debugging
+    console.log(main)
+};
+
+module.exports = async function run(content) {
+    // Parse the output from the AI
+    const lines = content.split("\n").map(line => line.trim());
+
+    action(lines);
+};
