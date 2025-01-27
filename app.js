90afbfc990a7ad676cdb8cbc0a658a10a11a60aa
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-27 03:25:05 +0000

    Added functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+const run = require("./src/runner");
+const { generateItems } = require("./src/generator/bin.pkg");
+const { writeConfig, configure } = require('./src/settings');
+
+async function defineApp() {
+    generateItems();
+    writeConfig();
+
+    const sendPrompt = configure();
+    // Add GROQ function logic
+}
