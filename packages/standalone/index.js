494f5d05c85b0da5d7b8763fbf9dc2c9fd2fca7c
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-08-29 16:21:08 +0700

    Create index.js

---
+++ packages/standalone/index.js
@@ -0,0 +1,20 @@
+/**=============================================================================================
+ *          ____  _                  _       _                       _                
+ *         / ___|| |_ __ _ _ __   __| | __ _| | ___  _ __   ___     / \   _ __  _ __  
+ *         \___ \| __/ _` | '_ \ / _` |/ _` | |/ _ \| '_ \ / _ \   / _ \ | '_ \| '_ \ 
+ *          ___) | || (_| | | | | (_| | (_| | | (_) | | | |  __/  / ___ \| |_) | |_) |
+ *         |____/ \__\__,_|_| |_|\__,_|\__,_|_|\___/|_| |_|\___| /_/   \_\ .__/| .__/ 
+ *                                                                       |_|   |_|    
+ * ==============================================================================================
+ * 
+ * Standalone App (Server Ver.) is an app developed by Cassitly for music lyrics sheet generation
+ * via a responsive UI system (instead of the previous developer based knowledge of editing generation files)
+ * 
+ * [PACKAGE INFORMATION]
+ * Ver. Information [str] (1.0-index.js)
+ * Build Information [str] (indev) as of [dated] 29/8/2025
+ * Dynamic Export [list] at import (avaliablity) as [function]
+ * Interface Information (1.0) imported from [link] (@cassitly.api/interfacing)
+**/
+
+// NOTE: build interpreter for package information (@cassitly.api/interfacing)
